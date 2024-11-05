68d344fc2aab8f0fdff1be71e0bb257156a3c2c1
Rakib Khan <srrakibkhan4@gmail.com>
2024-11-05 20:21:57 +0600

    dashboard change

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -8,71 +8,97 @@
     <div class="page-wrapper">
         <div class="content">
             <div class="row">
-                <div class="col-lg-3 col-sm-6 col-12">
-                    <a href="{{ route('all.batch') }}">
-                        <div class="dash-widget bg-primary">
+                <div class="col-lg-3 col-sm-6 col-12 mt-5">
+                    {{-- <a href="{{ route('all.batch') }}">
+                        <div class="dash-widget bg-primary align-center">
                             <div class="dash-widgetcontent">
                                 <h5 class="text-white">Batch Manager</h5>
                             </div>
                         </div>
-                    </a>
+                    </a> --}}
+
+                    <div class="page-btn">
+                        <a style="width: 250px" href="{{ route('all.batch') }}" class="btn btn-primary p-3">Batch Manager</a>
+                    </div>
                 </div>
-                <div class="col-lg-3 col-sm-6 col-12">
-                    <a href="{{ route('running.batch') }}">
+                <div class="col-lg-3 col-sm-6 col-12 mt-5">
+                    {{-- <a href="{{ route('running.batch') }}">
                         <div class="dash-widget bg-primary">
                             <div class="dash-widgetcontent">
                                 <h5 class="text-white">Running Batch</h5>
                             </div>
                         </div>
-                    </a>
+                    </a> --}}
+                    <div class="page-btn">
+                        <a style="width: 250px" href="{{ route('running.batch') }}" class="btn btn-primary p-3">Running
+                            Batch</a>
+                    </div>
                 </div>
-                <div class="col-lg-3 col-sm-6 col-12">
-                    <a href="{{ route('student.list') }}">
+                <div class="col-lg-3 col-sm-6 col-12 mt-5">
+                    {{-- <a href="{{ route('student.list') }}">
                         <div class="dash-widget bg-primary">
                             <div class="dash-widgetcontent">
                                 <h5 class="text-white">Student Manager</h5>
                             </div>
                         </div>
-                    </a>
+                    </a> --}}
+                    <div class="page-btn">
+                        <a style="width: 250px" href="{{ route('student.list') }}" class="btn btn-primary p-3">Student
+                            Manager</a>
+                    </div>
                 </div>
-                <div class="col-lg-3 col-sm-6 col-12">
-                    <a href="{{ route('chose.course') }}">
+                <div class="col-lg-3 col-sm-6 col-12 mt-5">
+                    {{-- <a href="{{ route('chose.course') }}">
                         <div class="dash-widget bg-primary">
                             <div class="dash-widgetcontent">
                                 <h5 class="text-white">Add Student</h5>
                             </div>
                         </div>
-                    </a>
+                    </a> --}}
+                    <div class="page-btn">
+                        <a style="width: 250px" href="{{ route('chose.course') }}" class="btn btn-primary p-3">Add
+                            Student</a>
+                    </div>
                 </div>
-                <div class="col-lg-3 col-sm-6 col-12">
-                    <a href="{{ route('add.payment') }}">
+                <div class="col-lg-3 col-sm-6 col-12 mt-5">
+                    {{-- <a href="{{ route('add.payment') }}">
                         <div class="dash-widget bg-primary">
                             <div class="dash-widgetcontent">
                                 <h5 class="text-white">Add Payment</h5>
                             </div>
                         </div>
-                    </a>
+                    </a> --}}
+                    <div class="page-btn">
+                        <a style="width: 250px" href="{{ route('add.payment') }}" class="btn btn-primary p-3">Add
+                            Payment</a>
+                    </div>
                 </div>
-                <div class="col-lg-3 col-sm-6 col-12">
-                    <a href="{{ route('expense.list') }}">
+                <div class="col-lg-3 col-sm-6 col-12 mt-5">
+                    {{-- <a href="{{ route('expense.list') }}">
                         <div class="dash-widget bg-primary">
                             <div class="dash-widgetcontent">
                                 <h5 class="text-white">Expenses</h5>
                             </div>
                         </div>
-                    </a>
+                    </a> --}}
+                    <div class="page-btn">
+                        <a style="width: 250px" href="{{ route('expense.list') }}" class="btn btn-primary p-3">Expenses</a>
+                    </div>
                 </div>
-                <div class="col-lg-3 col-sm-6 col-12">
-                    <a href="">
+                <div class="col-lg-3 col-sm-6 col-12 mt-5">
+                    {{-- <a href="">
                         <div class="dash-widget bg-primary">
                             <div class="dash-widgetcontent">
                                 <h5 class="text-white">All Dues</h5>
                             </div>
                         </div>
-                    </a>
+                    </a> --}}
+                    <div class="page-btn">
+                        <a style="width: 250px" href="" class="btn btn-primary p-3">Expenses</a>
+                    </div>
                 </div>
             </div>
-            <div class="row">
+            <div class="row mt-5">
                 <div class="col-lg-3 col-sm-6 col-12 d-flex">
                     <div class="dash-count">
                         <div class="dash-counts">
